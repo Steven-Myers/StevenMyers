67d4d5a7507aae51efc663020eeb8be20102da73
Steven Myers <50813742+Steven-Myers@users.noreply.github.com>
2022-07-26 10:41:31 -0500

    index updates and about

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// function headerPicChange(){
+
+//     document.getElementById("header-pic").innerHTML = "<img src='./media/images/header-2nd.png' style='height: auto; width: 100%;'>"
+//     document.getElementById("nav-title").innerHTML = ""
+// }
+
